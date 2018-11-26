fba33d9ec4b8d261a8cb6d3aeab42caf5a12ec03
JenniferZh <jenniferzhang0606@gmail.com>
2018-11-26 19:20:55 +0800

    schema import

---
+++ src/main/java/XmlSchemaParser.java
@@ -0,0 +1,15 @@
+
+class Element {
+    private String name;
+}
+
+class Attribute {
+    private String name;
+}
+
+public class XmlSchemaParser {
+
+    public XmlSchemaParser(String path) {
+
+    }
+}
